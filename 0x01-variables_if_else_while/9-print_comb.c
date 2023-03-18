144ceb15741566f47caf3a5707bb0401a2f4e990
tunde-ade <tunde.adeyemi09@outlook.com>
2023-03-18 11:05:59 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - c programm function
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
