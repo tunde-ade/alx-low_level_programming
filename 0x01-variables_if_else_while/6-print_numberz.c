0f312d1d9a744b15567b9a5ee66fe892a71399ab
tunde-ade <tunde.adeyemi09@outlook.com>
2023-03-18 10:31:55 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *  main - c programming function
+ *
+ *  Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
