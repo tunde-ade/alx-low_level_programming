7bfbab6b219a4df2f68a489d839128f14976d59f
tunde-ade <tunde.adeyemi09@outlook.com>
2023-03-18 02:34:29 -0700

    2-print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  main - main c function
+ *
+ *  Return: 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
