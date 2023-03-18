aeebe9db8b35547bcd8c3c03d6cc20f5d879844f
tunde-ade <tunde.adeyemi09@outlook.com>
2023-03-18 10:40:03 -0700

    print alphabet in rev

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - c programming function
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
