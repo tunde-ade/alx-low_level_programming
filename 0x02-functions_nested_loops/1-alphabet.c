f671a997a15dade417c833f8724aaf846d718554
tunde-ade <tunde.adeyemi09@outlook.com>
2023-03-21 23:50:41 -0700

    print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * main - print alphabet
+ * print_alphabet - print all alphabet
  *
  * Return: void
- */
+*/
+
 void print_alphabet(void)
 {
 	char ch;
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
-	_putchar(ch);
+		_putchar(ch);
+
 	_putchar('\n');
 }
