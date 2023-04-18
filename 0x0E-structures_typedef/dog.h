b636d913bee05130cd6c167fce0dc7721cb1d3b0
tunde-ade <tunde.adeyemi09@outlook.com>
2023-04-18 14:31:58 -0700

    a new typ dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,17 +8,20 @@
  * @owner: Third member
  * Description: Longer description
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 /**
  * dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
+
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
